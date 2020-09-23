f8cd96f45bf37957f29b6e2147c158d81da6cffd
Antonius <theantonius@gmail.com>
2020-09-23 18:00:59 -0400

    js functions example
    unit page that shows how to make functions in javascript

---
+++ unit4/functions/arguments.js
@@ -0,0 +1,11 @@
+/* This file shows how to send arguments into a function */
+
+function greet(name, greeting){
+	console.log(greeting + ", " + name);
+}
+
+// nothing will happen until we call the functions and send in the proper arguments.
+/*
+greet("Ruth", "Hello");
+greet("Ruth", "Good evening");
+*/
